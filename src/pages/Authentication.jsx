86f7c50e9ada9b6e73369325c3a7d573b98f0882
Garvit Chainani <garvit.chainani9@gmail.com>
2025-11-06 18:52:44 +0530

    Fix case-sensitive import path for Netlify build

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -1,4 +1,6 @@
-import LoginPage from "../components/loginPage/loginPage";
+// import LoginPage from "../components/loginPage/loginPage";
+
+import LoginPage from "../components/LoginPage/LoginPage";
 
 function Authentication() {
   return <LoginPage />;
